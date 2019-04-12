6a605995ba812427c3821ae4757dfd4b8fdbe6f2
Tom Spettigue <tom@adventinfosystems.com>
2019-04-12 14:48:28 -0600

    Got the map mostly working... dunno how I feel about that SVG not flowing properly, but we'll get that figured out

--- js/main.js
+++ js/main.js
@@ -1,10 +1,84 @@
 // Test that this damn script works
 console.log("Hello there!");
 
+// Object containing the data for the content for the modals
+// that appear on the front page map.
+//
+let mapModalContent = {
+
+  az: {
+    stateName: "Arizona",
+    svgPath: "",
+    cityList: [
+      "Tucson"
+    ]
+  },
+
+  ca: {
+    stateName: "California",
+    svgPath: "",
+    cityList: [
+      "Cupertino",
+      "Elk Grove - Highway 99",
+      "Elk Grove - Waterman",
+      "Milpitas",
+      "Sacramento - Expo",
+      "Sacramento - South"
+    ]
+  },
+
+  co: {
+    stateName: "Colorado",
+    svgPath: "",
+    cityList: [
+      "Aurora - Fitzsimons",
+      "Aurora - Havana"
+    ]
+  },
+
+  or: {
+    stateName: "Oregon",
+    svgPath: "",
+    cityList: [
+      "Salem"
+    ]
+  },
+
+  tx: {
+    stateName: "Texas",
+    svgPath: "",
+    cityList: [
+      "Aubrey",
+      "Fort Worth",
+      "Plano",
+      "Wylie"
+    ]
+  },
+
+  ut: {
+    stateName: "Utah",
+    svgPath: "",
+    cityList: [
+      "Layton",
+      "West Valley"
+    ]
+  },
+
+  wa: {
+    stateName: "Washington",
+    svgPath: "",
+    cityList: [
+      "Burien"
+    ]
+  }
+}
+
+
 // First attempt at making the fadeToggle() work
 //
 $(document).ready(function() {
   $("#CA").click(function() {
+    
     $(".map-modal").fadeToggle("fast");
   });
 });
